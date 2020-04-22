f1d08673d2337a3e144e7cc478fb9f42bade86bf
Ubuntu <ubuntu@witryjw-ml2-gpu-u18-04.us-east1-c.c.engaged-style-265300.internal>
2020-04-22 22:45:19 +0000

    Adding final model

--- witry_model.py
+++ witry_model.py
@@ -24,7 +24,7 @@ torch.backends.cudnn.benchmark = False
 
 # %% ----------------------------------- Hyper Parameters --------------------------------------------------------------
 LR = 1e-3
-N_EPOCHS = 50
+N_EPOCHS = 25
 BATCH_SIZE = 64
 DROPOUT = 0.2
 EPS = 1e-8
@@ -48,74 +48,12 @@ print("WEIGHT_DECAY", WEIGHT_DECAY)
 
 
 # from: https://pytorch.org/tutorials/beginner/data_loading_tutorial.html
-class RandomCrop(object):
-    """Crop randomly the image in a sample.
 
-    Args:
-        output_size (tuple or int): Desired output size. If int, square crop
-            is made.
-    """
-
-    def __init__(self, output_size):
-        assert isinstance(output_size, (int, tuple))
-        if isinstance(output_size, int):
-            self.output_size = (output_size, output_size)
-        else:
-            assert len(output_size) == 2
-            self.output_size = output_size
-
-    def __call__(self, sample):
-        image, landmarks = sample['image']
-
-        h, w = image.shape[:2]
-        new_h, new_w = self.output_size
-
-        top = np.random.randint(0, h - new_h)
-        left = np.random.randint(0, w - new_w)
-
-        image = image[top: top + new_h,
-                      left: left + new_w]
-
-
-        return {'image': image}
-
-
-class Rescale(object):
-    """Rescale the image in a sample to a given size.
-
-    Args:
-        output_size (tuple or int): Desired output size. If tuple, output is
-            matched to output_size. If int, smaller of image edges is matched
-            to output_size keeping aspect ratio the same.
-    """
-
-    def __init__(self, output_size):
-        assert isinstance(output_size, (int, tuple))
-        self.output_size = output_size
-
-    def __call__(self, sample):
-        image = sample
-
-        h, w = image.shape[:2]
-        if isinstance(self.output_size, int):
-            if h > w:
-                new_h, new_w = self.output_size * h / w, self.output_size
-            else:
-                new_h, new_w = self.output_size, self.output_size * w / h
-        else:
-            new_h, new_w = self.output_size
-
-        new_h, new_w = int(new_h), int(new_w)
-
-        img = transform.resize(image, (new_h, new_w))
-
-
-        return {'image': img}
 
 
 ### From https://discuss.pytorch.org/t/how-to-add-noise-to-mnist-dataset-when-using-pytorch/59745/2
 class AddGaussianNoise(object):
-    def __init__(self, mean=0., std=0.1):
+    def __init__(self, mean=0., std=0.05):
         self.std = std
         self.mean = mean
         
@@ -130,19 +68,53 @@ class AddGaussianNoise(object):
 
 
 ### transform.Normalize(torch.mean(
+class HammingFilter(object):
+    def __init__(self, r = 20):
+        self.r = r
+
+    def __call__(self, tensor):
+        tensor = tensor.numpy()
+        f = cv2.dft(np.float32(tensor[0,:,:]), flags=cv2.DFT_COMPLEX_OUTPUT)
+        f_shift = np.fft.fftshift(f)
+        f_complex = f_shift[:,:,0] + 1j*f_shift[:,:,1]
+        ham = np.hamming(224)[:, None]
+        ham2d = np.sqrt(np.dot(ham, ham.T)) ** self.r
+        f_filtered = ham2d*f_complex
+        f_filtered_shifted = np.fft.fftshift(f_filtered)
+        inv_img = np.fft.ifft2(f_filtered_shifted)  # inverse F.T.
+        filtered_img = np.abs(inv_img)
+        filtered_img -= filtered_img.min()
+        filtered_img = filtered_img * 255 / filtered_img.max()
+        filtered_img = filtered_img.astype(np.uint8)
+        im = np.zeros((224,224,3))
+        im[:,:,0] = filtered_img
+        im[:,:,1] = filtered_img
+        im[:,:,2] = filtered_img
+        print(im.shape)
+        print(filtered_img.shape)
+        #filtered_img_full = np.asarray([filtered_img,filtered_img,filtered_img]).reshape((224,224,3))
+        #print(filtered_img_full.shape)
+        im = Image.fromarray(im.astype(np.uint8))
+        im = transforms.functional.vflip(im)
+        return transforms.functional.to_tensor(im)
+
+
+
 
 ### Set Augmentations:
 
 data_transforms = {
     'train': transforms.Compose([
-        transforms.RandomResizedCrop(224,scale=(0.5,1.0)),
+        transforms.RandomAffine(30,scale=(0.8,1.2),shear=10),
         transforms.RandomHorizontalFlip(),
+        transforms.RandomResizedCrop(224,scale=(0.8,1)),
         transforms.ToTensor(),
-        #AddGaussianNoise(),
+        #HammingFilter(),
+        AddGaussianNoise(),
         transforms.Normalize([0.485,0.456,0.406],[0.229,0.224,0.225])
     ]),
     'val': transforms.Compose([
-        transforms.Resize(224),
+        transforms.Resize((224,224)),
         #transforms.CenterCrop(100),
         transforms.ToTensor(),
         transforms.Normalize([0.485,0.456,0.406],[0.229,0.224,0.225])
@@ -155,11 +127,26 @@ data_transforms = {
 def save_ckp(state, is_best, checkpoint_dir, best_model_dir):
     print("Saving checkpoint")
     if not is_best:
-        f_path = checkpoint_dir + 'checkpoint_NoGaussian.pt'
+        f_path = checkpoint_dir + 'checkpoint.pt'
         torch.save(state, f_path)
     else:
         f_path = best_model_dir + 'checkpoint_best_model4.pt'
         torch.save(state, f_path)
+
+def make_weights_for_balanced_classes(images, nclasses):                        
+    count = [0] * nclasses                                                      
+    for item in images:                                                         
+        count[item[1]] += 1
+    #for balanced batches
+    weight_per_class = [0.] * nclasses
+    N = float(sum(count))                                                   
+    for i in range(nclasses):                                                   
+        weight_per_class[i] = N/float(count[i]) 
+    #weight_per_class = [0.25,0.25,0.5]
+    weight = [0] * len(images)
+    for idx, val in enumerate(images):
+        weight[idx] = weight_per_class[val[1]]
+    return weight
 
 # %% -------------------------------------- Train ----------------------------------------------------------
 
@@ -207,8 +194,9 @@ def train_model(model, criterion, optimizer, scheduler=None, num_epochs=100):
                 # statistics
                 running_loss += loss.item() * inputs.size(0)
                 running_corrects += torch.sum(preds == labels.data)
-            if phase == 'train' and scheduler is not None:
-                scheduler.step()
+            if phase == 'train':
+                if scheduler is not None:
+                    scheduler.step()
                 ### Save checkpoints here
                 if (epoch + 1) % SAVE_EVERY_CHECKPOINT == 0:
                     checkpoint = {
@@ -253,9 +241,13 @@ def train_model(model, criterion, optimizer, scheduler=None, num_epochs=100):
 pneumonia_dataset = datasets.ImageFolder(root='../chest_xray_256x256/train',transform=data_transforms['train'])
 pneumonia_val_dataset = datasets.ImageFolder(root='../chest_xray_256x256/val',transform=data_transforms['val'])
 
+weights = make_weights_for_balanced_classes(pneumonia_dataset.imgs, len(pneumonia_dataset.classes))
+weights = torch.DoubleTensor(weights)
+sampler = torch.utils.data.sampler.WeightedRandomSampler(weights,len(weights),replacement=True)
+
 dataloaders = {}
-dataloaders['train'] = torch.utils.data.DataLoader(pneumonia_dataset,batch_size = BATCH_SIZE, shuffle = True, num_workers=0)
-dataloaders['val'] = torch.utils.data.DataLoader(pneumonia_val_dataset,batch_size = BATCH_SIZE, shuffle = True, num_workers=0)
+dataloaders['train'] = torch.utils.data.DataLoader(pneumonia_dataset, batch_size = BATCH_SIZE, sampler = sampler, num_workers=0)
+dataloaders['val'] = torch.utils.data.DataLoader(pneumonia_val_dataset, batch_size = BATCH_SIZE, shuffle = True, num_workers=0)
 
 dataset_sizes = {}
 dataset_sizes['train'] = len(pneumonia_dataset)
@@ -266,7 +258,12 @@ num_ftrs = model_ft.fc.in_features
 # Here the size of each output sample is set to 2.
 # Alternatively, it can be generalized to nn.Linear(num_ftrs, len(class_names)).
 model_ft.fc = nn.Linear(num_ftrs, 3)
-
+drop = nn.Dropout(DROPOUT)
+act = nn.LeakyReLU()
+#model_ft = nn.Sequential(model_ft,
+        #nn.LeakyReLU(),
+        #nn.Linear(4096,3))
+print(model_ft)
 model_ft = model_ft.to(device)
 
 criterion = nn.CrossEntropyLoss()
@@ -275,7 +272,7 @@ criterion = nn.CrossEntropyLoss()
 optimizer_ft = torch.optim.SGD(model_ft.parameters(), lr=LR, momentum=0.9)
 
 # Decay LR by a factor of 0.1 every 7 epochs
-exp_lr_scheduler = torch.optim.lr_scheduler.StepLR(optimizer_ft, step_size=7, gamma=0.1)
+#exp_lr_scheduler = torch.optim.lr_scheduler.StepLR(optimizer_ft, step_size=7, gamma=0.05)
 
-model_ft = train_model(model_ft, criterion, optimizer_ft, scheduler = exp_lr_scheduler,num_epochs=N_EPOCHS)
+model_ft = train_model(model_ft, criterion, optimizer_ft, scheduler = None,num_epochs=N_EPOCHS)
 
